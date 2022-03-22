92cfab1625af625550e2cccae5db36e1d79794ff
Eugene Anfalov <anfalovd10@yandex.ru>
2022-03-22 10:38:42 +0500

    Update dyynamic arrays.cpp

--- theory/dyynamic arrays.cpp
+++ theory/dyynamic arrays.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <cstdlib>
 
+using namespace std;
+
+int x, m[10000000];
+
+int main()
+{
+	int a;
+	int* p = (int*)malloc(3 * sizeof(int));
+	p[1] = 100;
+	cout << p[1] << endl;
+	free(p);
+	p = (int*)calloc(3, sizeof(int));
+	cout << p << endl;
+	p = (int*)realloc(p, 2 * sizeof(int));
+	cout << p << endl;
+	free(p);
+	p = NULL;
+
+	p = new int;
+	*p = 2;
+	cout << *p << endl;
+	delete p;
+	int n;
+	cin >> n;
+	p = new int[n];
+	p[n - 1] = 8;
+	delete[] p;
+}
