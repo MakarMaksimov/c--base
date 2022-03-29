b502f6b5a9a5a929d969f9b39b7284ed8d82612e
Eugene Anfalov <anfalovd10@yandex.ru>
2022-03-29 11:01:08 +0500

    Create 1580 k-е простое (решето).cpp

---
+++ tasks/informatics/1580 k-е простое (решето).cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void resheto(int n, bool **pr)
+{
+    *pr = new bool[n + 1];
+    for (int i = 0; i <= n; i++)
+        (*pr)[i] = 1;
+    (*pr)[0] = (*pr)[1] = 0;
+    for (int i = 2; i * i <= n; i++)
+        if ((*pr)[i])
+            for (int j = i * i; j <= n; j += i)
+                (*pr)[j] = 0;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int k, count = 0, i;
+    cin >> k;
+    bool* pr;
+    resheto(1500000, &pr);
+    for (i = 0; i <= 1500000 && count < k; i++)
+        count += pr[i];
+    cout << i - 1;
+}
